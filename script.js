571f51e24493f2b3751973c42c23696e4670415b
G Code <seanlfrancis@gmail.com>
2021-03-22 23:55:42 -0500

    Changed api call to one all inclusive. Still in process of declaring more weather conditions from data

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var apiKey= "f378bc500cb54543e663edad40c0f683";
 var houston= "houston";
-var url= `https://api.openweathermap.org/data/2.5/weather?q=${houston}&appid=${apiKey}&units=imperial`;
+// Back ticks needed for interpolated data
+var url= `https://api.openweathermap.org/data/2.5/onecall?lat=30.020630&lon=-95.424186&units=imperial&exclude=minutely,hourly,alerts&appid=${apiKey}`;
 var current = document.getElementById("current");
 var windDiv = document.getElementById("wind");
 var humidityDiv = document.getElementById("humidity");
@@ -8,18 +9,14 @@ var humidityDiv = document.getElementById("humidity");
 
 // fetch(url).then(res=>console.log(res));
 
-// fetch(url).then(function(response){
-//     return response.json();
-// })
-
 async function getWeather() {
     var response = await axios.get(url);
     console.log(response.data);
-    var currentTemp = response.data.main.temp;
+    var currentTemp = response.data.current.temp;
     current.innerHTML = currentTemp;
-    var wind = response.data.wind.speed;
+    var wind = response.data.current.wind_speed;
     windDiv.innerHTML = wind;
-    var humidity = response.data.main.humidity;
+    var humidity = response.data.current.humidity;
     console.log(humidity);
     humidityDiv.innerHTML = humidity;
 
